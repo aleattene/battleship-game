92779efa3bceb0fee1a89257b8bfec1ae2c8e0b1
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-11-26 11:13:51 +0100

    feat: added the function that generates the BattleShip board for each player

--- index.js
+++ index.js
@@ -21,19 +21,31 @@ function createEmptyBoard(dimension) {
     return board
 }
 
-let boardDimension = 5;
 
-board = createEmptyBoard(boardDimension)  // ?
-console.log(board);
-
-let player_01_positions = ["A4", "A2", "A3", "B4", "B5", "C1", "D3", "E5"]
-
-player_01_positions.forEach(element => modifyBoard(element));
-
-console.log(board);
-
-function modifyBoard(position){
+function modifyBoard(player_board, position){
     let row = position.charCodeAt(0) - 65
-    board[row][position[1]-1] = 1;
+    player_board[row][position[1]-1] = 1;
     // console.log(row)
 }
+
+
+function generateBoard(boardDimension, player_positions){
+    let board = createEmptyBoard(boardDimension)
+    player_positions.forEach(element => modifyBoard(board, element));
+    return board
+}
+
+let boardDimension = 5;
+let player_01_positions = ["A4", "A2", "A3", "B4", "B5", "C1", "D3", "E5"]
+let player_02_positions = ["A1", "A2", "B1", "B2", "B3", "C2", "D3", "E1"]
+
+// Player 01 Board
+let player_01_board = generateBoard(boardDimension, player_01_positions)
+
+// Player 02 Board
+let player_02_board = generateBoard(boardDimension, player_02_positions)
+
+console.log(player_01_board)
+console.log(player_02_board)
+
+

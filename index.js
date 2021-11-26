37bf0f94862d219ab1b84f66953b16de0b836d03
Alessandro Attene <74595044+aleattene@users.noreply.github.com>
2021-11-26 15:16:03 +0100

    feat: added simulation of game for one player

--- index.js
+++ index.js
@@ -14,38 +14,66 @@ function createEmptyBoard(dimension) {
     for (let i = 0; i < dimension; i++) {
         let row = [];
         for (let j = 0; j < dimension; j++) {
-            row.push(0)
+            row.push(0);
         }
-        board.push(row)
+        board.push(row);
     }
-    return board
+    return board;
 }
 
 
-function modifyBoard(player_board, position){
-    let row = position.charCodeAt(0) - 65
-    player_board[row][position[1]-1] = 1;
+function modifyBoard(player_board, position, isShip){
+    let row = position.charCodeAt(0) - 65;
+    player_board[row][position[1]-1] = isShip;
     // console.log(row)
 }
 
 
 function generateBoard(boardDimension, player_positions){
-    let board = createEmptyBoard(boardDimension)
-    player_positions.forEach(element => modifyBoard(board, element));
-    return board
+    let board = createEmptyBoard(boardDimension);
+    player_positions.forEach(element => modifyBoard(board, element, 1));
+    return board;
 }
 
 let boardDimension = 5;
-let player_01_positions = ["A4", "A2", "A3", "B4", "B5", "C1", "D3", "E5"]
-let player_02_positions = ["A1", "A2", "B1", "B2", "B3", "C2", "D3", "E1"]
+let player_01_positions = ["A4", "A2", "A3", "B4", "B5", "C1", "D3", "E5"];
+let player_02_positions = ["A1", "A2", "B1", "B2", "B3", "C2", "D3", "E1"];
 
 // Player 01 Board
-let player_01_board = generateBoard(boardDimension, player_01_positions)
+let player_01_board = generateBoard(boardDimension, player_01_positions);
 
 // Player 02 Board
-let player_02_board = generateBoard(boardDimension, player_02_positions)
-
-console.log(player_01_board)
-console.log(player_02_board)
+let player_02_board = generateBoard(boardDimension, player_02_positions);
 
 
+
+console.log(player_01_board);
+console.log(player_02_board);
+
+
+function fire(player_defender_positions, player_defender_board, player_attacker, player_attacker_move){
+    if (player_defender_positions.includes(player_attacker_move)) {
+        player_defender_positions.splice(player_defender_positions.indexOf(player_attacker_move), 1);
+        modifyBoard(player_defender_board, player_attacker_move, 0);
+        console.log("BOOM");
+        console.log(player_defender_positions);
+        if (player_defender_positions.length === 0){
+            console.log(`Game over. The winner is ${player_attacker}`)
+        }
+    } else {
+        console.log("splash");
+    }
+}
+
+
+fire(player_02_positions, player_02_board, "Player 01",  "A1");
+fire(player_02_positions, player_02_board, "Player 01",  "A2");
+fire(player_02_positions, player_02_board, "Player 01",  "B1");
+fire(player_02_positions, player_02_board, "Player 01",  "B2");
+fire(player_02_positions, player_02_board, "Player 01",  "B3");
+fire(player_02_positions, player_02_board, "Player 01",  "C2");
+fire(player_02_positions, player_02_board, "Player 01",  "A1");
+fire(player_02_positions, player_02_board, "Player 01",  "D3");
+fire(player_02_positions, player_02_board, "Player 01",  "E1");
+
+console.log(player_02_board);
